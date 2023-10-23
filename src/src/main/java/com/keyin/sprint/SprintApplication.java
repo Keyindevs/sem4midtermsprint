226b6f4da097f1212566d9655c3ae3b71c4124e2
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-10-23 14:50:42 -0230

    Initial versioning of files

---
+++ src/src/main/java/com/keyin/sprint/SprintApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintApplication.class, args);
+	}
+
+}
