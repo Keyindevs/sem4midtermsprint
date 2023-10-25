405e8fefe983da103c1b8cd9e9354acd0611180a
AbbyOConnell <116213683+AbbyOConnell@users.noreply.github.com>
2023-10-25 18:46:05 -0230

    Revert "City class"

--- src/src/main/java/com/keyin/sprint/entities/City.java
+++ src/src/main/java/com/keyin/sprint/entities/City.java
@@ -1,49 +1,4 @@
 package com.keyin.sprint.entities;
 
 public class City {
-    private int id;
-    private String name;
-    private String state;
-    private int population;
-
-    public City(int id, String name, String state, int population) {
-        this.id = id;
-        this.name = name;
-        this.state = state;
-        this.population = population;
-    }
-
-    // Getters and setters for the fields
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public int getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(int population) {
-        this.population = population;
-    }
 }
