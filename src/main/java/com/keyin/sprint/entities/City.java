1dec2daf16e3a1202d7e857136689ab7220e2883
Riley <riley.ledrew@keyin.com>
2023-10-25 20:03:44 -0230

    fixed src file and moved it to root of project

--- src/main/java/com/keyin/sprint/entities/City.java
+++ src/main/java/com/keyin/sprint/entities/City.java
@@ -1,53 +1,19 @@
 package com.keyin.sprint.entities;
 
-import java.util.ArrayList;
-
 public class City {
     private int id;
     private String name;
     private String state;
     private int population;
 
-    private final ArrayList<Airport> airports;
-    private final ArrayList<Passenger> habitants = new ArrayList<>();
-
-    public City(int id, String name, String state, int population, ArrayList<Airport> airports) {
-        this.id = id;
-        this.name = name;
-        this.state = state;
-        this.population = population;
-        this.airports = airports;
-    }
-
     public City(int id, String name, String state, int population) {
         this.id = id;
         this.name = name;
         this.state = state;
         this.population = population;
-        this.airports = new ArrayList<>();
-    }
-
-    public String display() {
-        return String.format("%s, ID: %d, State: %s, Population: %d", this.name, this.id, this.state, this.population);
     }
 
     // Getters and setters for the fields
-
-    public ArrayList<Passenger> getHabitants() {
-        return habitants;
-    }
-
-    public void addHabitant(Passenger passenger) {
-        this.habitants.add(passenger);
-    }
-
-    public void addAirport(Airport airport) {
-        this.airports.add(airport);
-    }
-
-    public ArrayList<Airport> getAirports() {
-        return airports;
-    }
 
     public int getId() {
         return id;
