455da078221f8f4a98d1c37724ba3f8f8346f185
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-10-25 19:56:22 -0230

    api routes

--- src/src/main/java/com/keyin/sprint/entities/Passenger.java
+++ src/src/main/java/com/keyin/sprint/entities/Passenger.java
@@ -2,31 +2,15 @@ package com.keyin.sprint.entities;
 
 public class Passenger {
     // need attributes of firstname last name hometown and an id
-    private final String firstName;
-    private final String lastName;
-    private final String homeTown;
-    private final int id;
+    private String firstName;
+    private String lastName;
+    private String homeTown;
+    private int id;
 
     public Passenger(String firstName, String lastName, String homeTown, int id) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.homeTown = homeTown;
         this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getHomeTown() {
-        return homeTown;
-    }
-
-    public int getId() {
-        return id;
     }
 }
