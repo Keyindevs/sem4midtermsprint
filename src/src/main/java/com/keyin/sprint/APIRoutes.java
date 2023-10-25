5aafae00b9fddc53f1b48987e499a5689ea87920
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-10-25 19:56:22 -0230

    api routes

--- src/src/main/java/com/keyin/sprint/APIRoutes.java
+++ src/src/main/java/com/keyin/sprint/APIRoutes.java
@@ -2,11 +2,12 @@ package com.keyin.sprint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class APIRoutes {
 	public static void main(String[] args) {
@@ -25,5 +26,16 @@ public class APIRoutes {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "This is a test!";
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to the Sprint API!";
+	}
+
 }
 
