52bde362f2e107e21f331876594a63f8cedc431f
AbbyOConnell <116213683+AbbyOConnell@users.noreply.github.com>
2023-10-25 21:40:06 -0230

    Testing of the passenger class
    Testing of the passenger class and fixed something in the passenger class

--- src/src/main/java/com/keyin/sprint/entities/Passenger.java
+++ src/src/main/java/com/keyin/sprint/entities/Passenger.java
@@ -2,15 +2,31 @@ package com.keyin.sprint.entities;
 
 public class Passenger {
     // need attributes of firstname last name hometown and an id
-    private String firstName;
-    private String lastName;
-    private String homeTown;
-    private int id;
+    private final String firstName;
+    private final String lastName;
+    private final String homeTown;
+    private final int id;
 
     public Passenger(String firstName, String lastName, String homeTown, int id) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.homeTown = homeTown;
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getHomeTown() {
+        return homeTown;
+    }
+
+    public int getId() {
+        return id;
     }
 }
---
+++ src/src/test/java/com/keyin/sprint/PassengerTest.java
@@ -0,0 +1,17 @@
+package com.keyin.sprint;
+
+import com.keyin.sprint.entities.Passenger;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PassengerTest {
+    @Test
+    public void testPassengerAttributes() {
+        Passenger passenger = new Passenger("John", "Doe", "New York", 12345);
+
+        assertEquals("John", passenger.getFirstName());
+        assertEquals("Doe", passenger.getLastName());
+        assertEquals("New York", passenger.getHomeTown());
+        assertEquals(12345, passenger.getId());
+    }
+}
