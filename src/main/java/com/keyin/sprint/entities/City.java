f8db87df3e8341d8d5eaa35ca592c13109f8ae68
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-10-26 14:20:09 -0230

    Progress on cities and airports routes

--- src/main/java/com/keyin/sprint/entities/City.java
+++ src/main/java/com/keyin/sprint/entities/City.java
@@ -1,19 +1,40 @@
 package com.keyin.sprint.entities;
 
+import java.util.ArrayList;
+
 public class City {
     private int id;
     private String name;
     private String state;
     private int population;
 
+    private final ArrayList<Airport> airports;
+
+    public City(int id, String name, String state, int population, ArrayList<Airport> airports) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+        this.population = population;
+        this.airports = airports;
+    }
+
     public City(int id, String name, String state, int population) {
         this.id = id;
         this.name = name;
         this.state = state;
         this.population = population;
+        this.airports = new ArrayList<>();
     }
 
     // Getters and setters for the fields
+
+    public void setAirport(Airport airport) {
+        this.airports.add(airport);
+    }
+
+    public ArrayList<Airport> getAirports() {
+        return airports;
+    }
 
     public int getId() {
         return id;
