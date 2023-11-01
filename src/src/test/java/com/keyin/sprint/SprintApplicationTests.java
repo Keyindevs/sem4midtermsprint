568e20ff94c4813c65d8c99cf7088a579d50ab5f
AbbyOConnell <116213683+AbbyOConnell@users.noreply.github.com>
2023-11-01 01:19:16 -0230

    Update SprintApplicationTests.java
    Stinky

--- src/src/test/java/com/keyin/sprint/SprintApplicationTests.java
+++ src/src/test/java/com/keyin/sprint/SprintApplicationTests.java
@@ -1,13 +1,13 @@
-package com.keyin.sprint;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SprintApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.keyin.sprint;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SprintApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
